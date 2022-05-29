d1eda123a25ebbe38a8e080fbc39c5a5575840ef
jonas-martinez <cbobbxta.fr@gmail.com>
2022-05-29 18:45:33 +0200

    Documenting

--- listeners/onEnvStart.js
+++ listeners/onEnvStart.js
@@ -5,6 +5,10 @@ const userService = require("../services/userService");
 
 
 module.exports = (props, event, api) => {
+    // Create user datastore to store user infos (username, ...)
     userService.createDatastore(api).catch((e => {  }));
+    // Create game datastore to store game infos
+    // TODO gameService
+
     return counterService.createDatastore(api).catch((e => { }));
 }
