8bb29cadc37319c6096c4d2957b7f3cbff15b2e3
jonas-martinez <cbobbxta.fr@gmail.com>
2022-06-20 17:10:03 +0200

    More work

--- services/userGameService.js
+++ services/userGameService.js
@@ -29,28 +29,13 @@ module.exports = {
         ).then((value) => value.data);
     },
     async new(api, newUserGame, userId) {
-        let all = await this.get(api, newUserGame.appid);
-        console.log("CREATING NEW USER GAME");
-        console.log(all);
-        // TODO: Check if user already has game
-
-
-        // let exists = false;
-        // let existingUserGame = {};
-        // all.data.map(userGame => {
-        //     if (userGame.appid == newUserGame.appid) {
-        //         exists = true;
-        //         existingUserGame = userGame;
-        //     }
-        // })
-        // if (!exists) {
-        //     return await axios.post(`${api.url}/app/datastores/games/data`, { ...newUserGame, "userIds": [userId] }, { headers: { Authorization: `Bearer ${api.token}` } });
-        // } else {
-        //     if (existingUserGame.userIds.indexOf(userId) === -1) {
-        //         existingUserGame.userIds.push(userId);
-        //     }
-        //     return await this.put(api, existingUserGame);
-        // }
+        let userGame = await this.get(api, newUserGame.appid);
+        if(userGame === undefined) {
+            return await axios.post(`${api.url}/app/datastores/userGames/data`, { ...newUserGame, "userId": userId }, { headers: { Authorization: `Bearer ${api.token}` } });
+        } else {
+            // TODO: Put user game 
+            // return await this.put(api, userId, )
+        }
     },
     put(api, userId, data) {
         return axios.put(`${api.url}/app/datastores/userGames/data/${userId}`, data, headers(api));
