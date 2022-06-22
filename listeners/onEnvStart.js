bbf29e6e6e4d5b2f9e32c85d637ecb76ee9cefec
jonas-martinez <cbobbxta.fr@gmail.com>
2022-06-22 15:45:04 +0200

    Handle achievements for users

--- listeners/onEnvStart.js
+++ listeners/onEnvStart.js
@@ -1,14 +1,16 @@
 'use strict'
 
 const gameService = require('../services/gameService');
+const userGameService = require('../services/userGameService');
 
 module.exports = async (props, event, api) => {
     console.log("On Env Start");
     console.log(props);
     console.log(event);
     console.log(api);
-    // TODO: Create "games" datastore
+    
     await gameService.createDatastore(api);
+    await userGameService.createDatastore(api);
 
     return {};
 }
--- services/steamService.js
+++ services/steamService.js
@@ -2,12 +2,13 @@
 
 const { default: axios } = require("axios");
 const gameService = require('../services/gameService');
+const userGameService = require("./userGameService");
 
 const steamAPIKey = "CA8F42C8DC798264FCF69D088BB2E918";
 const steamAPIBaseUrl = "https://api.steampowered.com";
 
 module.exports = {
-    getUserSteamIDFromVanity(api, userId, vanityUrl) {
+    getUserSteamIDFromVanity(vanityUrl) {
         return axios.get(`${steamAPIBaseUrl}/ISteamUser/ResolveVanityURL/v0001/?key=${steamAPIKey}&vanityurl=${vanityUrl}`).then((value) => value.data.response);
     },
     async getUserGames(api, userId, steamId) {
@@ -15,25 +16,35 @@ module.exports = {
         let promises = [];
         games.games.forEach(game => {
             if (game !== undefined) {
-                promises.push(gameService.new(api, game, userId));
-                this.getUserGameAchievements(api, userId, steamId, game.appid);
+                promises.push(new Promise(async () => {
+                    // Add game in datastore
+                    await gameService.new(api, game, userId);
+                    // Add achievements to game
+                    let achievements = await this.getGameAchievements(steamId, game.appid)
+                    await gameService.put(api, { ...game, achievements: achievements });
+                    // Add user game achievements in datastore
+                    let unlockedAchievements = await this.getUserGameAchievements(api, userId, steamId, game.appid);
+                    // TODO: Also add play time
+                    await userGameService.new(api, { appid: game.appid, achieved: unlockedAchievements }, userId)
+                }));
             }
         });
         return await Promise.all(promises);
     },
-    async getUserGameAchievements(api, userId, steamId, gameId) {
+    async getGameAchievements(steamId, gameId) {
+        // Get user game achievements as there is not API to get game achievements directly
         let achievements = await axios.get(`${steamAPIBaseUrl}/ISteamUserStats/GetPlayerAchievements/v0001/?key=${steamAPIKey}&steamid=${steamId}&appid=${gameId}`).then((value) => value.data.playerstats.achievements);
-        console.log("GET USER GAME ACHIEVEMENTS");
-        console.log(achievements);
-        let unlockedAchievementsNumber = achievements.reduce(function(acc, cur) {
-            return acc + cur.achieved;
-        }, 0);
+        // Return array of achievements apiname
+        return achievements.map(achievement => achievement.apiname);
+    },
+    async getUserGameAchievements(steamId, gameId) {
+        // Get user achievements (unlocked & locked)
+        let achievements = await axios.get(`${steamAPIBaseUrl}/ISteamUserStats/GetPlayerAchievements/v0001/?key=${steamAPIKey}&steamid=${steamId}&appid=${gameId}`).then((value) => value.data.playerstats.achievements);
 
-        let game = gameService.get(api, gameId);
-        // TODO: Create new datastore UserGame to handle user specific game information such as unlocked achievements
-        // userGameService.put(api, game);
-        
-        return achievements;
+        // Filter unlocked achievements
+        let unlocked = achievements.filter(achievement => achievement.achieved == 1 ? true : false);
+
+        return unlocked.map(achievement => achievement.apiname);
     },
     put(api, userId, data) {
         return axios.put(`${api.url}/app/datastores/_users/data/${userId}`, data, headers(api));
