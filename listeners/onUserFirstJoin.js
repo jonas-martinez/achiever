bb3f9f79f336e905c1b9d2bf88f2fb996d1706c6
jonas-martinez <cbobbxta.fr@gmail.com>
2022-10-13 15:24:12 +0200

    feat: Preparing mongo migration

--- listeners/onUserFirstJoin.js
+++ listeners/onUserFirstJoin.js
@@ -3,10 +3,6 @@
 const userService = require("../services/userService");
 
 module.exports = async (props, event, api) => {
-    console.log("On User First Join");
-    console.log(props);
-    console.log(event);
-    console.log(api);
     var userData = await userService.get(api);
     await userService.put(api, userData._id, {
         ...userData,
