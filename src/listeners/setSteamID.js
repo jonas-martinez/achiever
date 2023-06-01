4527b6792c8fd152dbe4da136eb537af08d84f9b
jonas-martinez <cbobbxta.fr@gmail.com>
2023-06-02 00:01:53 +0200

    Add webhook methods

--- src/listeners/setSteamID.js
+++ src/listeners/setSteamID.js
@@ -1,5 +1,6 @@
 'use strict'
 
+import axios from "axios";
 import userService from "../services/userService.js";
 import navigate from "../listeners/navigator.js";
 
@@ -15,13 +16,22 @@ export default async function (props, event, api) {
     userData.steamId = event.value.steamId;
     await userService.update(api, userData);
 
-    let res = await api.data.createWebhook(api, {
+    let res = await createWebhook(api, {
         action: "getUserGames"
     })
 
-    await api.data.triggerWebhook(api, res.data.uuid, { userId: userData.id, steamId: userData.steamId });
+    await triggerWebhook(api, res.data.uuid, { userId: userData.id, steamId: userData.steamId });
 
     await navigate({ page: "homePage" }, {}, api);
 
     return {};
 }
+
+function createWebhook(api, params) {
+    return axios.post(`${api.url}/app/webhooks`, params, options(api)).catch((error) => console.log(error));
+};
+
+function triggerWebhook(api, uuid, props) {
+    // TODO: How to get app uuid ?
+    return axios.post(`${api.url}/apps/:app_uuid/webhooks/${uuid}`, props, options(api)).catch((error) => console.log(error));
+};
